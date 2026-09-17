e90688e32137aff6bccc179129cc62fadd728b14
aninhalu2 <analudossantos222@gmail.com>
2026-09-17 15:05:17 -0300

    Update main.php

---
+++ main.php
@@ -0,0 +1,7 @@
+<?php
+require_once__DIR__ . '/functions/server.php';
+require_once__DIR__ . '/router/router.php';
+require_once__DIR__ . '/middlewere/middlewere.php'; 
+require_once__DIR__ . '/dispatcher/dispatcher.php';
+require_once__DIR__ . '/controllers/usuario_controller.php';
+require_once__DIR__ . '/services/usuarios_service.php';
